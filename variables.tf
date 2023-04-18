39d69a80a17832dcb9c9105817e79ef2ade7e117
Terraform <romain.cambonie@gmail.com>
2023-04-18 15:34:20 +0200

    feat: adding terraform aws variables file

--- variables.tf
+++ variables.tf
@@ -1,3 +1,16 @@
+variable "terraform_organization" {
+  type        = string
+  description = "The organization name on terraform cloud"
+  nullable    = false
+}
+
+variable "tfe_token" {
+  description = "TFE Team token"
+  nullable    = false
+  default     = false
+  sensitive   = true
+}
+
 variable "project" {
   type        = string
   nullable    = false
