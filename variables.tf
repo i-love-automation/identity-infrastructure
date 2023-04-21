311f3b22c1723a3fe58ff3bcc31ad339587d107e
Terraform <noreply@codingones.com>
2023-04-21 11:14:58 +0200

    feat: adding terraform aws variables file

--- variables.tf
+++ variables.tf
@@ -23,6 +23,14 @@ variable "service" {
   description = "The name of the service that will be run on the environment"
 }
 
+variable "domain_name" {
+  type        = string
+  nullable    = false
+  description = "The project registered domain name that cloudfront can use as aliases, for now only one domain is supported"
+  default     = ""
+}
+
+# todo passer en map pour faire un foreach avec les propriétés du groupe)
 variable "user_group_admin" {
   type        = string
   nullable    = false
@@ -34,4 +42,11 @@ variable "user_group_user" {
   nullable    = false
   description = "The user group user name"
   default     = "user"
+}
+
+variable "ses_configuration_set_name" {
+  type        = string
+  nullable    = false
+  description = "The ses email configuration set name"
+  default     = ""
 }
